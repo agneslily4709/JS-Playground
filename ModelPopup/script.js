f2e30f8c910c45e09c51aaa075a56ca903557c37
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-09-11 23:38:26 +0530

    Add Modal Popup

--- ModelPopup/script.js
+++ ModelPopup/script.js
@@ -1,15 +1,22 @@
-const modal = document.getElementById("modal")
+var modal = document.getElementById("myModal");
 
-const close = document.getElementById("close-button")
-close.addEventListener("click",function(){
-        modal.style.display = "none"
-})
+var btn = document.getElementById("myBtn");
 
-const button = document.getElementById("clickButton")
-button.addEventListener("click",function(){
-        modal.style.display = "block"
-        modal.style.zIndex = 1
-        modal.style.alignItems = "center"
-})
+var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}       
