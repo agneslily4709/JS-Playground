08ca1a0139abe10b94227fa2e083f58ed2ba1423
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-09-15 22:39:21 +0530

    Final Commit

--- GroceryList/script.js
+++ GroceryList/script.js
@@ -1,28 +1,51 @@
-const groceryList = []
-const form = document.getElementById("form")
-const formInput = document.getElementById("formInput")
-let item = ""
-formInput.addEventListener("input",function(){
-        item = formInput.value
-})
-form.addEventListener("submit",function(e){
-        e.preventDefault()
-        console.log(item)
-        groceryList.push(item)
-        formInput.value = ""
-        renderPage()
-})
-var renderPage = function(){
-        const content = document.getElementById("content")
-        for (let grocery of groceryList){
-                let itemDiv = document.createElement("div")
-                let itemTitle = document.createElement("p")
-                let editBtn = document.createElement("button")
-                editBtn.textContent = "Edit"
-                let delBtn = document.createElement("button")
-                delBtn.textContent = "Delete"
-                itemTitle.textContent = grocery
-                itemDiv.append(itemTitle,editBtn,delBtn)
-                content.append(itemDiv)
-        }
+const groceryList = [{ id: 1, item: "bread" }, { id: 2, item: "milk" }];
+const form = document.getElementById("form");
+const formInput = document.getElementById("formInput");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    addItemToList(formInput.value);
+    formInput.value = "";
+});
+
+function renderPage() {
+    const content = document.getElementById("content");
+    content.innerHTML = "";
+
+    groceryList.forEach((item) => {
+        const itemDiv = document.createElement("div");
+        itemDiv.className = "itemDiv"
+        const itemTitle = document.createElement("p");
+        itemTitle.className = "itemTitle"
+        const delBtn = document.createElement("button");
+        delBtn.className = "delBtn"
+        delBtn.innerHTML = "&#10060;";
+        itemTitle.textContent = item.item;
+        delBtn.addEventListener("click", () => {
+            deleteItem(item.id);
+            renderPage();
+        });
+
+        itemDiv.append(itemTitle, delBtn);
+        content.append(itemDiv);
+    });
 }
+
+function addItemToList(itemName) {
+    const newItem = {
+        id: groceryList.length + 1,
+        item: itemName,
+    };
+
+    groceryList.push(newItem);
+    renderPage(); 
+}
+
+function deleteItem(itemId) {
+    const index = groceryList.findIndex((item) => item.id === itemId);
+
+    if (index !== -1) {
+        groceryList.splice(index, 1);
+    }
+}
+renderPage();
